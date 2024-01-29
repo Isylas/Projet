e1611337d646847f75dcd98bcb1c15f034390f5d
Titourien <titoutate@gmail.com>
2024-01-29 09:06:22 +0100

    tout bon

--- Splines.py
+++ Splines.py
@@ -44,8 +44,6 @@ def splines_cubiques(x, y):
 
 def résolution(x, y):
     matFinale, matRes = splines_cubiques(x, y)
-    print("Matrice finale avant inversion:")
-    print(matFinale)
     matFinale_inv = np.linalg.inv(matFinale)
     matCoeff = np.dot(matFinale_inv, matRes)
     return matCoeff
@@ -55,26 +53,37 @@ def dérivée_polynome (poly): # dérive un polynome de degré 3 (4 coeff)
     a=poly[3]
     b=poly[2]
     c=poly[1]
-    return [c, 2*b, 3*a]
+    return [c, 2*b, 3*a,0]
+
+def dérivée_seconde(poly):
+    a=poly[3]
+    b=poly[2]
+    return([2*b,6*a,0,0])
         
 def dérivée_spline(x,y):  #renvoie la dérivée des splines sous la forme de matrice 3*(n-1)
     n=len(x)
-    dérivée_spline=[]
+    dérivée1_spline=[]
     matCoeff = résolution(x,y)
+    print(matCoeff)
     for i in range (n-1):
+        dérivée=[]
         poly = [matCoeff[4*i][0],matCoeff[4*i+1][0],matCoeff[4*i+2][0],matCoeff[4*i+3][0]]
-        dérivée_spline.append(dérivée_polynome (poly))    
-    return dérivée_spline
+        dérivée=dérivée_polynome (poly)
+        for j in range(4):
+            dérivée1_spline.append([dérivée[j]])
+    return dérivée1_spline
 
 def dérivée_seconde_spline(x,y):
     n=len(x)
-    dérivée_seconde_spline=[]
-    dérivée_première=dérivée_spline(x,y)
-    for i in range(n-1):
-        a=dérivée_première[i][2]
-        b=dérivée_première[i][1]
-        dérivée_seconde_spline.append([b,2*a])
-    return dérivée_seconde_spline
+    dérivée_2_spline=[]
+    matCoeff = résolution(x,y)
+    for i in range (n-1):
+        dérivée=[]
+        poly = [matCoeff[4*i][0],matCoeff[4*i+1][0],matCoeff[4*i+2][0],matCoeff[4*i+3][0]]
+        dérivée=dérivée_seconde(poly)
+        for j in range(4):
+            dérivée_2_spline.append([dérivée[j]])
+    return dérivée_2_spline
 
 def equilibre(x,y):
 
@@ -82,8 +91,8 @@ def equilibre(x,y):
     res = None
     dérivée_seconde= dérivée_seconde_spline(x,y)
     for i in range (n-1):
-        a=dérivée_seconde[i][1]
-        b= dérivée_seconde[i][0]
+        a=dérivée_seconde[i+1]
+        b=dérivée_seconde[i]
         if -b/a >= x[i] and -b/a <= x[i+1]:
             res = -b/a
     return res
@@ -92,14 +101,62 @@ def equilibre(x,y):
 
 x=[0.4,0.8,1.2,1.6,2,2.4,2.8,3.2,3.6,4,4.4,4.8,5.2,5.6,6,6.4,6.8,7.2,7.6,8,8.4,8.8,9.2,9.6,10,10.4,10.8,11.2,11.6,12,12.4,12.8,13.2,13.6,14,14.4,14.8,15.2,15.6,16,16.4,16.8,17.2,17.6,18,18.4,18.8,19.2,19.6,20]
 y=[1.42265,1.4276960317453,1.43312785633646,1.43899754166588,1.44536020444814,1.45228060739777,1.45983536352571,1.46811577308034,1.47723151530769,1.48731551094278,1.49853041197118,1.51107739025696,1.52520823252366,1.5412422861543,1.55959068121755,1.58079174169147,1.60556409358411,1.63488867277774,1.67013969426102,1.7133021834278,1.76735038908997,1.83694347140276,1.92979307030677,2.05958216452858,2.25286268280524,2.56749349899594,3.14835947756378,4.37874149918824,6.59218667954964,8.35619449019234,9.19109346720768,9.61086747893003,9.85423465523273,10.011159437778,10.120202300835,10.2001833459588,10.2612746963633,10.3094246545258,10.3483329786724,10.380417316863,10.4073224250955,10.4302052765007,10.4499029579775,10.4670358385307,10.4820733065754,10.4953769887689,10.5072299496132,10.5178568977251,10.5274384684669,10.5361215122375]
-# x=[1,2,3,4,5,6]
-# y=[1,3,6,9,1,-2]
-
-print (résolution(x,y))
-
-
-print(dérivée_seconde_spline(x,y))
-
+# x=[3.5,
+# 4,
+# 4.3,
+# 4.6,
+# 4.7,
+# 4.9,
+# 5.1,
+# 5.3,
+# 5.6,
+# 5.9,
+# 6.5,
+# 7,
+# 8,
+# 9,
+# 10,
+# 11,
+# 12,
+# 13,
+# 14,
+# 14.5,
+# 14.6,
+# 14.7,
+# 14.8,
+# 14.9,
+# 15,
+# 15.05
+# ]
+# y=[1.62,
+# 1.77,
+# 1.9,
+# 2.09,
+# 2.17,
+# 2.38,
+# 2.72,
+# 3.12,
+# 3.41,
+# 3.67,
+# 3.95,
+# 4.11,
+# 4.36,
+# 4.56,
+# 4.75,
+# 4.94,
+# 5.14,
+# 5.38,
+# 5.7,
+# 6.11,
+# 6.24,
+# 6.34,
+# 6.65,
+# 6.98,
+# 10.1,
+# 10.65,
+# ]
+# x=[1,2,3,4]
+# y=[2,3,4,5]
 
 def affichage(X,Y,res):
     x=symbols('x')
@@ -115,6 +172,11 @@ def affichage(X,Y,res):
 
 
 
-res=résolution (x,y)
+res=résolution(x,y)
+res=dérivée_spline(x,y)
+print(dérivée_spline(x,y))
+affichage(x,y,res)
+res=dérivée_seconde_spline(x,y)
+print(dérivée_seconde_spline(x,y))
 affichage(x,y,res)
 
